73f363f13f9b7b929275b3fb721ade9e90372f08
Vanness Yang <vanness.yang21@ycdsbk12.ca>
2021-01-03 20:56:07 +0000

    Changes in Bag

--- src/vkickz/Bag.java
+++ src/vkickz/Bag.java
@@ -21,15 +21,23 @@ public class Bag extends Item{
     this.season = theSeason;
   }
 
+  /**
+  * Getter for Bag Type   
+  */
   public String getBagType(){
     return bagType;
   }
 
+  /**
+  * Getter for Season of the Bag  
+  */
   public String getSeason(){
     return season;
   }
 
-  
+  /**
+  * toString method to return values of variables 
+  */
   public String toString(){
     return "The " + this.getItemType() + " available is the " + this.getBrand() + " " + season + " " + this.getColourway() + " " + bagType + ". The starting price is $" + this.getPrice();
   }
