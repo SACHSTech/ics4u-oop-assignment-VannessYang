b8d10e1994aab3f7852eddffdb1184082333f35b
Vanness Yang <vanness.yang21@ycdsbk12.ca>
2021-01-03 22:14:14 +0000

    Changes to Store

--- src/vkickz/Store.java
+++ src/vkickz/Store.java
@@ -3,6 +3,9 @@ package vkickz;
 import vkickz.*; 
 import java.util.ArrayList;
 
+/**
+* Store class
+*/
 public class Store {
   
   // Attributes
@@ -11,8 +14,13 @@ public class Store {
   private Bag bag;
   private Shoe shoe;
 
-  
-  public Store(Staff theStaff, Bag theBag, Shoe theShoe, int numCustomer){
+  /**
+  * Constructor - creates a new store instance
+  * @param theStaff - store has staff
+  * @param theBag - store has bags
+  * @param theShoe - store has shoes
+  */
+  public Store(Staff theStaff, Bag theBag, Shoe theShoe){
     this.staff = theStaff;
     this.bag = theBag;
     this.shoe = theShoe;
